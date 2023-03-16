d5e2b337e54acbe2ce5542015b11f094e8dc9677
Tapsmutyaaz <tapsmutyason@gmail.com>
2023-03-16 13:29:41 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,10 @@
 #include <unistd.h>
 
 /**
-* main - print quote
-*
-* Description: print Dora's quote
-* Return: Always 1 (Success)
+* main - Entry
+* Return: 1
 */
-
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 559)"
-return (1);
-}
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)"
+return (1);}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
 /**
-* You should produce the exact same output as in the example
-* Warnings are allowed
-*
-* Your program should return 0
-*/
-int main(void) 
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of a int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of a int: %i byte(s)\n", sizeof(int));
+	printf("size of a long: %i byte(s)\n", sizeof(long int));
+	printf("size of a long long: %i byte(s)\n", sizeof(long long int));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
-}
+
