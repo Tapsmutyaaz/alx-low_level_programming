0fa8c7730013686681b7611ce95c8c37109bd178
Tapsmutyaaz <tapsmutyason@gmail.com>
2023-03-16 19:01:44 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,5 @@ printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+return (0):
 }
