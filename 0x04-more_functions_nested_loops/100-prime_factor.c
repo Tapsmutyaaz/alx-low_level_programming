882dd59d015598a71dd5defb98d9f885f935c32d
Tapsmutyaaz <tapsmutyason@gmail.com>
2023-03-23 14:05:57 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,40 +1,49 @@
-#include <stdio.h>
-#include <math.h>
+#include "main.h"
 
 /**
- * main - finds and prints the largest prime factor of the number 612852475143
- * followed by a new line
- * Return: Always 0 (Success)
+ * print_triangle - prints a triangle.
+ * @size: numbers of lines.
+ * Return: no return.
  */
-int main(void)
+void print_triangle(int size)
 {
- long int n;
- long int max;
- long int i;
+	int i, j;
 
- n = 612852475143;
- max = -1;
-
- while (n % 2 == 0)
- {
- max = 2;
- n /= 2;
- }
-
- for (i = 3; i <= sqrt(n); i = i + 2)
- {
- while (n % i == 0)
- {
- max = i;
- n = n / i;
- }
- }
-
- if (n > 2)
- max = n;
-
- printf("%ld\n", max);
-
- return (0);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
 }
 
+Task 11. 100-prime_factor.c
+
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n, fp;
+
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
+	{
+		if (n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
+
+	}
+	printf("%ld\n", fp);
+	return (0);
+}
